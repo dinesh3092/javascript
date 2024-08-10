4f7771af339f7a2600716941c51f469073d125d9
dinesh3092 <151391876+dinesh3092@users.noreply.github.com>
2024-08-10 10:45:28 +0000

    variables in js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,7 @@ const accountId = 1992
 let accountEmail = "dineshmaheshwari25@gmail.com"
 var accountPassword = "12345"
 accountCity = "Ganganagar"
-
+let accountState
 
 // accountId = 12 // not allowed
 
@@ -11,4 +11,4 @@ accountPassword = "54321"
 accountCity = "Bathinda"
 
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
