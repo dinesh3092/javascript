2669064c05f52a0583b497b4a15d195c3995383d
dinesh3092 <151391876+dinesh3092@users.noreply.github.com>
2024-08-10 10:41:08 +0000

    variables used in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1992
+let accountEmail = "dineshmaheshwari25@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ganganagar"
+
+
+// accountId = 12 // not allowed
+
+accountEmail = "dinesh@gmail.com"
+accountPassword = "54321"
+accountCity = "Bathinda"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
