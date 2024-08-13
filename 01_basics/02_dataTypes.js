4d4ae5d566ded0151d024162e02baebab7b2bd34
dinesh3092 <151391876+dinesh3092@users.noreply.github.com>
2024-08-13 06:29:48 +0000

    js data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+//  old javascript and new javascript are different.
+
+"use strict"; // treat all js code as newer version
+
+// alert() directly not used in node js.
+
+let name = "Dinesh" // String data type
+let age = 30  // Number data type
+let isLoggedIn = true; // Boolean data type
+let state; // undefined data type
+let city = null // null data type
+
+// Primitive data type
+// number   => 2 to power 53
+// bigint use in trading like websites
+// string  => "Dinesh"
+// boolean  =>  True/False
+// null  => standalone value
+// undefined  => value not defined
+// symbol  => unique use in react to create components
+
+
+// Non primitive data type
+
+// object 
+
+console.log(typeof "Dinesh"); // type string
+console.log(typeof null); // type object
+console.log(typeof undefined); // type undefined
+
+
