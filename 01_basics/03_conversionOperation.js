51124398d9667b674c776ef8a0e9888115ac02d7
dinesh3092 <151391876+dinesh3092@users.noreply.github.com>
2024-08-13 11:02:15 +0000

    js Operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -43,3 +43,23 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+
+
+/**************Operations ************************** */
+console.log("Operations");
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = ""
+
