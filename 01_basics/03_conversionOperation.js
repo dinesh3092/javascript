602de0e0244d9e7ca27fdd694c021e42d4ca1118
dinesh3092 <151391876+dinesh3092@users.noreply.github.com>
2024-08-14 10:24:26 +0000

    Operator and Conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -61,5 +61,56 @@ console.log(negValue);
 // console.log(2/2);
 // console.log(2%3);
 
-let str1 = ""
+let str1 = "Hello"
+let str2 = " Dinesh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 - 2 + "2");
+
+console.log(3+4*5/2); // Good for Question but not good for real coding
+
+console.log(true);
+console.log(+true);
+//console.log(true+); // Error
+
+console.log(false);
+console.log(+false);
+//console.log(false+); // Error
+
+console.log(+"");
+console.log(+" ");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2 // confusing not used in real coding
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+// Presend Operator
+
+/** Postfix Operator */
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+/** Prefix Operator*/
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
 
