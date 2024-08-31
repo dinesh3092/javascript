5ebbbd78bae063314ee3c89bdba431233b76f671
dinesh3092 <151391876+dinesh3092@users.noreply.github.com>
2024-08-31 09:16:39 +0000

    complete js code

---
+++ projects/countdown /js/app.js
@@ -0,0 +1,77 @@
+const endDate = "02 September 2024 12:00 AM"
+//const endDate = "31 August 2024 02:46 PM"
+
+// date show dynamically on page
+document.getElementById('end-Date').innerText = endDate;
+const inputs = document.querySelectorAll("input");
+// console.log(inputs);
+
+
+// Arrow Function
+const clock = () => {
+    const end = new Date(endDate);
+    const now = new Date();
+
+    // console.log(`End Date is ${end}`);
+    // console.log(`Current Date is ${now}`); 
+
+    //convert milliseconds into seconds   
+    const difference = (end - now)/1000;
+
+    //console.log(`Difference between Dates is ${difference}`);
+
+
+
+
+    // convert difference into days
+    const days = Math.floor(((difference/3600))/24)
+    inputs[0].value = days;
+    //console.log(days)
+    // console.log(difference/3600/24)
+
+
+
+
+    // convert difference into hours
+    const hours = Math.floor(((difference/3600)%24));
+    // console.log(((difference/3600)%24))
+    // console.log(hours);
+    inputs[1].value = hours;
+
+
+
+    // convert difference into minutes
+    const min = Math.floor((difference/60)%60)
+    //console.log(min)
+    inputs[2].value = min;
+
+    //convert difference into minutes
+    const sec = Math.floor(difference) % 60;
+    inputs[3].value = sec;
+
+    if(days == 0 && hours == 0 && min == 0 && sec == 0)
+    {
+        stopFunction();
+    }
+}
+
+clock();
+
+// Calculation
+/**
+ *  1 day = 24 hrs
+ *  1 hr = 60 min
+ *  1 hr = 60min * 60seconds = 3600 seconds
+ * 
+ */
+
+
+const timeInterval = setInterval(() => {
+    clock()
+}, 1000
+);
+
+const stopFunction = () =>{
+    clearInterval(timeInterval);
+}
+
