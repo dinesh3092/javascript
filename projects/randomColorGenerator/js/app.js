464b0db27c256bba919a3a53aee9526903d22595
dinesh3092 <151391876+dinesh3092@users.noreply.github.com>
2024-08-31 12:28:16 +0000

    complete

--- projects/randomColorGenerator/js/app.js
+++ projects/randomColorGenerator/js/app.js
@@ -1,10 +1,26 @@
 const getColor = () =>{
 
-    const randomNumber = Math.random();
-    console.log(randomNumber);
+    const randomNumber = Math.floor(Math.random() * 16777216);
+    const randomCode = "#" + randomNumber.toString(16); // 16 for hex code converter
+    
+    
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById('color-code').innerText = randomCode;
 }
 
+// event call
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+);
+ 
+
+//init call
 getColor();
 
+// setInterval(() =>{
+//     getColor()   
+// }, 5000
+// );
 // Random Number
 // Hex Code 
